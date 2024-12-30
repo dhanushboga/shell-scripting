bb04f08a8e6053b3406a3e6809d651de1795cd27
dhanushboga <dhanushboga555@gmail.com>
2024-12-30 15:46:38 +0530

    variables script

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=dhanush
+person2=kumar
+
+echo "hello $person1 : How are you"
+echo "Hi $person2 : I am fine hope you are doing well"
+echo "$person2 : how is your devops practice"
+echo "$person1 : it's good and I am learning to get the new devops role"
