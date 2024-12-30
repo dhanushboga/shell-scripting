0a498260ffda3996f9d49e7297d566bf50f445a9
dhanushboga <dhanushboga555@gmail.com>
2024-12-30 16:09:10 +0530

    functions script

--- functions.sh
+++ functions.sh
@@ -2,9 +2,12 @@
 
 echo "it's time to do addition using functions"
 
+echo "Enter the first Number:" $1
+echo "Enter the second Number:" $2
+
 sum(){
     sum=$(($1+$2))
     echo "sum of $1 and $2 :" $sum
 }
 
-sum 5 6
+sum $1 $2
