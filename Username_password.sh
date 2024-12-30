171484c81951d1bc096c1cc5e4b32d9a469eafe8
dhanushboga <dhanushboga555@gmail.com>
2024-12-30 15:38:17 +0530

    username and password script

---
+++ Username_password.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please provide your User Name"
+read USERNAME
+
+echo "Enter your password"
+read -s PASSWORD
+
+echo "enter user name :" $USERNAME
+echo "password :" $PASSWORD
