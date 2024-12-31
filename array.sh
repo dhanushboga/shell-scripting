324fc40e7d9b6683d19ab8cafd6c2ded1c9f5478
dhanushboga <dhanushboga555@gmail.com>
2024-12-31 15:28:50 +0530

    arrays script

--- array.sh
+++ array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 array=("bat" "ball" "wickets")
-echo "first entered is :$array[0]"
-echo "second entered is : $array[1]"
-echo "third entered is : $array[2]"
-echo "all the items are : $array[@]"
+echo "first entered is :${array[0]}"
+echo "second entered is : ${array[1]}"
+echo "third entered is : ${array[2]}"
+echo "all the items are : ${array[@]}"
