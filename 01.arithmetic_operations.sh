d6fb582d5e1b604e8ac42868c967c13dc61cd3ed
dhanushboga <dhanushboga555@gmail.com>
2025-01-02 19:52:35 +0530

    arithmetic operations using functions

--- 01.arithmetic_operations.sh
+++ 01.arithmetic_operations.sh
@@ -5,40 +5,25 @@ echo "it's time to do Arithmetic Operations using functions by passing arguments
 echo "Enter the first Number:" $1
 echo "Enter the second Number:" $2
 
-sum(){
+Arithmetic(){
     sum=$(($1+$2))
     echo "sum of $1 and $2 :" $sum
-}
-
-sum $1 $2
-
-sub(){
+    
     sub=$(($1-$2))
     echo "sub 0f $1 and $2 :" $sub
-}
-
-sub $1 $2
-
-mul(){
+    
     mul=$(($1*$2))
     echo "Mul of $1 and $2 : " $mul
-}
-
-mul $1 $2
-
-Div(){
+    
     div=$(($1/$2))
     echo "Div of $1 and $2 : " $div
-}
-
-Div $1 $2
-
-mod(){
+    
     mod=$(($1%$2))
     echo "Mod of $1 and $2 : " $mod
+
 }
 
-mod $1 $2
+Arithmetic $1 $2
 
 echo "wow it's completed the arithmetic operations for $1 and $2"
 
