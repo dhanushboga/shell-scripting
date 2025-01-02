4830319cc846bbbd339c2cd1b45f23e0393dfb72
dhanushboga <dhanushboga555@gmail.com>
2025-01-02 20:33:24 +0530

    colours script

--- colours.sh
+++ colours.sh
@@ -25,7 +25,7 @@ dnf list installed mysql
 
 if [ $? -ne 0 ]
 then
-  dnf install mysql -y
+  dnf install mysqll -y
   VALIDATE $? "installing MYSQL"
 else
   echo -e "MYSQL is already.......$Y installed"
