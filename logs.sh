d4c9440b0108d412e50efcc8f94cede0df91b832
dhanushboga <dhanushboga555@gmail.com>
2025-01-02 20:52:50 +0530

    logs script

---
+++ logs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+LOG_FOLDER="/var/log/shell_script_logs"
+LOG_FILE=$(echo $0|cut -d '.' -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIME_STAMP.log
+
+USER_ID=$(id -u)
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[0m'
+
+if [ $USER_ID -ne 0 ]
+then
+  echo "error :: please run this script under superuser access"
+  exit 1;
+fi
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+  then
+    echo -e "$2..........$R FAILURE $N"
+  else
+    echo -e "$2..........$G Success $N"
+fi
+}
+
+dnf list installed mysql &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+  dnf install mysql -y
+  VALIDATE $? "Installing MYSQL"
+else
+  echo -e "MYSQL is already.......$Y installed"
+fi
+
+dnf list installed git &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+  dnf install git -y &>>$LOG_FILE_NAME
+  VALIDATE $? "Installing GIT"
+else
+  echo -e "GIT is already.........$Y installed"
+fi
