7af12f62ad45181b1c028a65ec2c7623944bc446
dhanushboga <dhanushboga555@gmail.com>
2025-01-05 18:20:21 +0530

    longest_word

--- count_of_words.sh
+++ count_of_words.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-file="C:\Users\Bogad\devops\repos\shell-scripting\long_file.txt"
+file="\Users\Bogad\devops\repos\shell-scripting\long_file.txt"
 file_content=$(cat $file)
 
 echo "The file content is $file_content"
@@ -10,7 +10,7 @@ for word in $file_content
 do
 Length_of_each_word=$(echo $word | tr -d " \t\n\r" | wc -m)
 echo "Length of $word : $Length_of_each_word"
-done
+done >>>$file
 
 count_of_words=$(echo $file_content | wc -w)
 echo "the Total number of words is $count_of_words"
