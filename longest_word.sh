be2fb752e14cb2f0645ca0cb9fa394e52fa5753a
dhanushboga <dhanushboga555@gmail.com>
2025-01-05 17:24:29 +0530

    longest_word

--- longest_word.sh
+++ longest_word.sh
@@ -3,5 +3,6 @@
 echo "enter the file Name :"
 read file
 
-word=$(echo -n "$file" | wc -w)
-echo "The total number of words is $word"
+file_content=$(cat $file)
+
+echo "The file content is $file_content"
