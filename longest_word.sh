be01ee7248ce0db6094c2936d9d614e2ab1855dc
dhanushboga <dhanushboga555@gmail.com>
2025-01-05 17:42:52 +0530

    longest_word

--- longest_word.sh
+++ longest_word.sh
@@ -9,7 +9,7 @@ echo "The file content is $file_content"
 
 for word in $file_content
 do
-Length_of_each_word=$(echo $word | wc -m)
+Length_of_each_word=$(echo $word | cut -d ''-f1 | wc -m)
 echo "Length of the word : $Length_of_each_word"
 
 count_of_words=$(echo $file_content | wc -w)
