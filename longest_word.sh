4811875b891c9b8da5cf401271fd8351f15c287d
dhanushboga <dhanushboga555@gmail.com>
2025-01-05 17:27:32 +0530

    longest_word

--- longest_word.sh
+++ longest_word.sh
@@ -6,3 +6,7 @@ read file
 file_content=$(cat $file)
 
 echo "The file content is $file_content"
+
+count_of_words=$(echo $file_content | wc -l)
+
+echo "the Total number of words is $count_of_words"
