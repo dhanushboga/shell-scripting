81842dd7e8d3239feb86dd8b8730454c366f9ad1
dhanushboga <dhanushboga555@gmail.com>
2025-01-06 19:01:23 +0530

    reverse content script

---
+++ content_reverse.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+reverse_content_file="\home\ec2-user\reverse-file"
+
+file=long_file.txt
+
+file_content=$(cat $file)
+echo "The file content is \n $file_content"
+
+mkdir -p $
+
+rev_content=$(tac $file_content) $>> $reverse_content_file
+echo "The reverse content is \n $rev_content"
+
+
+
+
