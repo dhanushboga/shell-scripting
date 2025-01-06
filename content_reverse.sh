f921a33637f09d555ae77cce1c19d99402aa132f
dhanushboga <dhanushboga555@gmail.com>
2025-01-06 19:10:47 +0530

    reverse content script

--- content_reverse.sh
+++ content_reverse.sh
@@ -7,7 +7,7 @@ file=long_file.txt
 file_content=$(cat $file)
 echo "The file content is : $file_content"
 
-rev_content=$(echo $file_content | tac $file_content )
+rev_content=$(echo $file_content | tac )
 
 echo "The reverse content is : $rev_content"
 
