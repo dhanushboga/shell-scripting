cab616680efd123db0ee581ff877b664c6202c96
dhanushboga <dhanushboga555@gmail.com>
2025-01-06 19:03:52 +0530

    reverse content script

--- content_reverse.sh
+++ content_reverse.sh
@@ -7,7 +7,7 @@ file=long_file.txt
 file_content=$(cat $file)
 echo "The file content is \n $file_content"
 
-mkdir -p $
+mkdir -p $reverse_content_file
 
 rev_content=$(tac $file_content) $>> $reverse_content_file
 echo "The reverse content is \n $rev_content"
